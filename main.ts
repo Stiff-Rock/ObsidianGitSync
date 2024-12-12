eec8f99e7842aa6b658e56eeff917469caf1050c
Yago <yaguitor@gmail.com>
2024-12-12 13:26:07 +0100

    pull, push, addAndCommit and auth remaining

--- main.ts
+++ main.ts
@@ -1,85 +1,37 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, EventRef, PluginSettingTab, App, Setting, Notice } from 'obsidian';
+import { start } from 'repl';
+import simpleGit, { SimpleGit } from 'simple-git';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface GitSettings {
+	gitHubRepo: string;
+	gitHubUser: string;
+	gitHubPwd: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: GitSettings = {
+	gitHubRepo: '',
+	gitHubUser: '',
+	gitHubPwd: ''
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+//TODO: Add periodic commiting system, then push everything at the end
+
+export default class ObsidianGitSync extends Plugin {
+	settings: GitSettings;
+	git: SimpleGit = simpleGit((this.app.vault.adapter as any).basePath);
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('Hello Obsidian!!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		if (await this.git.checkIsRepo())
+			await this.fetchVault();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		//this.startGitTimer()
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new GitSyncSettingTab(this.app, this));
 	}
 
-	onunload() {
-
+	async onunload() {
 	}
 
 	async loadSettings() {
@@ -89,46 +41,155 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async on(name: 'quit', callback: (tasks: Tasks) => any, ctx?: any): EventRef {
+		console.log('BYEBYE')
+	}
+
+	startGitTimer() {
+		console.log('Git timer started')
+		setInterval(async () => {
+			await this.addAndCommitVault();
+		}, 3000);
+	}
+
+	async createRepo() {
+		let message = 'Unknown error initializing repository'
+
+		try {
+			const isRepo = await this.git.checkIsRepo();
+			if (!isRepo) {
+				await this.git.init();
+				//TODO: Check if the remote is correct
+				await this.git.addRemote('origin', this.settings.gitHubRepo);
+				message = "Git repository initialized";
+			} else {
+				message = "Repository already exists";
+			}
+		} catch (error) {
+			console.error("Error initializing Git repo:", error);
+		} finally {
+			new Notice(message, 3000);
+		}
+	}
+
+	async addAndCommitVault() {
+		if (await this.git.checkIsRepo()) {
+			try {
+				this.git.add('.')
+
+				const date = new Date().toISOString();
+				this.git.commit('Changes at ' + date)
+				//FIX: I dont think its adding and commiting 
+				console.log('Commit - ' + date);
+			} catch (error) {
+				let message = "Error adding and commiting chagnes"
+				console.error(message + ": ", error);
+				new Notice(message, 3000);
+			}
+		} else {
+			console.log('No repo in this vault')
+		}
+	}
+
+	async pushVault() {
+		try {
+
+		} catch (error) {
+			let message = "Error pushing chagnes to repository"
+			console.error(message + ": ", error);
+			new Notice(message, 3000);
+		}
+	}
+
+	async fetchVault() {
+		try {
+			await this.git.fetch()
+			const status = await this.git.status();
+
+			if (status.behind > 0) {
+				console.log(`Local is behind by ${status.behind} commit(s).`);
+				console.log('Pulling...')
+
+				try {
+					await this.git.pull()
+				} catch (error) {
+					let message = "Error pulling from remote"
+					console.error(message + ": ", error);
+					new Notice(message, 3000);
+				}
+			}
+		} catch (error) {
+			let message = "Error fetching from remote"
+			console.error(message + ": ", error);
+			new Notice(message, 3000);
+		}
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class GitSyncSettingTab extends PluginSettingTab {
+	plugin: ObsidianGitSync;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ObsidianGitSync) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('GitHub Repository Url')
+			.setDesc('The Url of the GitHub repository that will store the vault')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Repository Url')
+				.setValue(this.plugin.settings.gitHubRepo)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.gitHubRepo = value;
 					await this.plugin.saveSettings();
-				}));
+				})
+				.inputEl.classList.add('git-sync-config-field')
+			);
+
+		new Setting(containerEl)
+			.setName('GitHub Username')
+			.setDesc('Your GitHub username')
+			.addText(text => text
+				.setPlaceholder('Username')
+				.setValue(this.plugin.settings.gitHubUser)
+				.onChange(async (value) => {
+					this.plugin.settings.gitHubUser = value;
+					await this.plugin.saveSettings();
+				})
+				.inputEl.classList.add('git-sync-config-field')
+			);
+
+		new Setting(containerEl)
+			.setName('GitHub Password/Token')
+			.setDesc('Password or personal access token')
+			.addText((text) => {
+				text
+					.setPlaceholder('Password or token')
+					.setValue(this.plugin.settings.gitHubPwd)
+					.onChange(async (value) => {
+						this.plugin.settings.gitHubPwd = value;
+						await this.plugin.saveSettings();
+					})
+				text.inputEl.classList.add('git-sync-config-field');
+				text.inputEl.setAttribute("type", "password");
+			});
+
+		new Setting(containerEl)
+			.setName('Create vault repository')
+			.setDesc('Creates the local repository if not done yet')
+			.addButton((button) => {
+				button.setButtonText('Create repository')
+				button.onClick(_ => this.plugin.createRepo())
+				button.buttonEl.classList.add('git-sync-config-field')
+			})
+
 	}
 }
+
