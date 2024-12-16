6ffb49d82968fe4a0495aa94aab8db148a1511b5
Yago <yaguitor@gmail.com>
2024-12-16 11:06:02 +0100

    merged nffm into main

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
-import { Plugin, PluginSettingTab, App, Setting, Notice, Tasks, TextComponent, ButtonComponent, ToggleComponent } from 'obsidian';
+import { Plugin, PluginSettingTab, App, Setting, Notice, Tasks, TextComponent, ButtonComponent, ToggleComponent, Modal } from 'obsidian';
 import simpleGit, { SimpleGit } from 'simple-git';
 import * as fs from 'fs';
 import * as path from 'path';
-import { NonFastForwardModal } from './NonFastForwardModal';
 
 class GitSettings {
 	/*NOTE: Maybe add sepparate boolean values for the strings since you want to store 
@@ -797,3 +796,33 @@ class GitSyncSettingTab extends PluginSettingTab {
 	}
 }
 
+class NonFastForwardModal extends Modal {
+	constructor(app: App, onSubmit: (result: boolean) => void) {
+		super(app);
+		this.setTitle('A conflict has ocurred');
+		this.contentEl.innerHTML = `
+			<p>Your vault has changes that are not in sync with the uploaded version.</p>
+			<ul>
+				<li>Press "Confirm" to pull the latest changes and apply your updates on top. The remote changes won’t be visible directly, and you may need to manually add them to your vault.</li>
+				<li>Press "Cancel", no changes will be pushed, and you’ll need to resolve the conflict manually (this is not recommended unless you are familiar with Git).</li>
+			</ul>
+		`;
+
+		new Setting(this.contentEl)
+			.addButton(button => {
+				button.setButtonText('Accept')
+					.onClick(() => {
+						this.close();
+						onSubmit(true);
+					});
+
+			})
+			.addButton(button => {
+				button.setButtonText('Cancel')
+					.onClick(() => {
+						this.close();
+						onSubmit(false);
+					});
+			});
+	}
+}
